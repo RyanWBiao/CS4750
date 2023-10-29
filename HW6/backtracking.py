43284e9c3f0ea719e9b2af4114059853ebf5c839
RyanWBiao <bwpzz@umsystem.edu>
2023-10-29 10:21:23 -0500

    HW6 state and its methods

---
+++ HW6/backtracking.py
@@ -0,0 +1,25 @@
+from typing import List,Tuple
+import copy
+import time
+from classes import State,Variable
+
+test_puzzle = [
+    [1,0,0,2,5,0,6,0,8],
+    [0,4,0,0,8,0,0,5,0],
+    [5,0,8,0,0,0,0,0,2],
+    [0,0,0,0,4,0,8,0,0],
+    [4,0,0,8,0,1,0,6,0],
+    [0,5,0,0,2,6,0,0,0],
+    [7,0,4,1,6,2,5,0,0],
+    [6,0,5,4,7,0,1,2,3],
+    [2,0,0,5,3,8,7,0,0]
+]
+
+test_state = State(test_puzzle,[])
+
+test_state.print_board()
+
+for variable in test_state.get_variables():
+    print(variable.var_coordinate(),end=" ")
+    print(variable.var_priority())
+
--- HW6/classes.py
+++ HW6/classes.py
@@ -1,4 +1,4 @@
-from typing import List,Tuple
+from typing import List
 import copy
 
 # Define variable object including:
@@ -6,9 +6,14 @@ import copy
 # 2. Priority calculated by (constraints count) and (remaining value count)
 # 3. Coordinate
 class Variable:
-    def __init__(self,coordinate:Tuple(int,int),priority:int) -> None:
+    def __init__(self,coordinate:tuple,priority:int) -> None:
         self.coordinate = coordinate
         self.priority = priority
+
+    def var_coordinate(self):
+        return self.coordinate
+    def var_priority(self):
+        return self.priority
 
 # 1. An array of current distribution
 # 2. Variable List
@@ -17,15 +22,16 @@ class Variable:
 # 4. Sort variable function
 class State:
     def __init__(self,board:List[List[int]],Assginments:List):
-        self.board = board
+        self.board = [[0] * 10 for _ in range(10)]  # Creating a 2D list
+        # Add a margin to the board:
+        for i in range(9):
+            self.board[i + 1] = [0] + board[i]
         self.assignments = Assginments
-        # self.variables = self.get_variables(self)
-
-    def get_block_number(x:int,y:int):
-        return ((x//3)*3+(y//3)+1)
+        self.variables = self.get_variables()
 
     # The input should be the coordinate of a variable and borad, and this method return its priority
-    def get_priority(coordinate:Tuple(int,int),board:List[List[int]]):
+    def get_priority(self,coordinate:tuple):
+        board = self.board
         x = coordinate[0]
         y = coordinate[1]
         remain_values = [1,2,3,4,5,6,7,8,9]
@@ -38,14 +44,25 @@ class State:
         remain_values_count = len(remain_values)
 
         # Calculate the degree:
+        degree = 0
+        # 1. The constraint to the valriables in the same row
         for i in range(1,10):
-            if i!= y and board[x][i] in remain_values:
-                remain_values.remove(board[x][i])
+            if board[x][i] == 0:
+                degree = degree + 1
+        # 2. The constraint to the valriables in the same col
         for i in range(1,10):
-            if i!= x and board[i][y] in remain_values:
-                remain_values.remove(board[i][y])
-
-        pass    
+            if board[i][y] == 0:
+                degree = degree + 1
+        # 3. The constraint to the valriables in the same block
+        block_start_x = ((x-1)//3)*3+1
+        block_start_y = ((y-1)//3)*3+1
+        for i in range(block_start_x,block_start_x+3):
+            for j in range(block_start_y,block_start_y+3):
+                if i != x and j != y and board[i][j] == 0:
+                    degree = degree + 1
+        
+        # Return the priority value: (lower value means higher priority)
+        return remain_values_count * 10000 + degree * 100 + y*10 + x
     
     # Get all the variables that have not been assigned values yet. And put them into a list.
     def get_variables(self) -> List[Variable]:
@@ -53,12 +70,25 @@ class State:
         variables = []
         for i in range(1,10):
             for j in range(1,10):
-                if self.board[i][j] != 0:
-                    new_variable = Variable((i,j),self.get_priority((i,j),self.board))
+                if self.board[i][j] == 0:
+                    new_variable = Variable((i,j),self.get_priority((i,j)))
                     variables.append(new_variable)
+        return variables
+    
 
-    def print_board():
-        pass
-    pass
+    def print_board(self):
+        for i in range(1,10):
+            for j in range(1,10):
+                if self.board[i][j] == 0:
+                    print(" ",end=" ")
+                else:
+                    print(self.board[i][j],end=" ")
+                if j%3 == 0:
+                    print("|",end='')
+            if i%3 == 0:
+                print("")
+                print("---------------------")
+            else:
+                print("")
     
 
